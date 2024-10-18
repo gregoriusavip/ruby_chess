b53d1ed5445159f0a625905d47cf17e61481f9ea
gregoriusavip <71865513+gregoriusavip@users.noreply.github.com>
2024-10-17 22:37:56 -0700

    Add files/ranks indicator for the board output

--- lib/board/board_output.rb
+++ lib/board/board_output.rb
@@ -2,21 +2,28 @@
 
 # Handles board visualization
 module BoardOutput
-  def print_board
-    order = 1
-    @chessboard.each do |ranks|
-      ranks.each do |piece|
-        print("#{piece.nil? ? colored_square(order) : piece.piece} ")
-        order += 1
+  def print_chessboard
+    counter = 1
+    @chessboard.each_with_index do |ranks, rank_index|
+      print "#{8 - rank_index} "
+      ranks.each_with_index do |piece, order|
+        print("#{piece.nil? ? alt_square(order + counter) : piece.piece} ")
       end
-      puts
-      order -= 1
+      print "\n"
+      counter -= 1
     end
+    print_chessboard_files
   end
 
   private
 
-  def colored_square(order)
+  def print_chessboard_files
+    print '  '
+    8.times { |i| print("#{('a'.ord + i).chr} ") }
+    puts
+  end
+
+  def alt_square(order)
     (order % 2).eql?(0) ? '□' : '■'
   end
 end
