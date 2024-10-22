0869b1977a5fd609383e63366032fe935c61072c
gregoriusavip <71865513+gregoriusavip@users.noreply.github.com>
2024-10-21 23:25:27 -0700

    Modify to use ChessParser to set the board

--- lib/chessboard/chessboard.rb
+++ lib/chessboard/chessboard.rb
@@ -2,12 +2,6 @@
 
 require_relative('output')
 require_relative('../parser')
-require_relative('../pieces/pawn')
-require_relative('../pieces/rook')
-require_relative('../pieces/knight')
-require_relative('../pieces/queen')
-require_relative('../pieces/king')
-require_relative('../pieces/bishop')
 
 # A standard chessboard
 class Chessboard
@@ -34,24 +28,17 @@ class Chessboard
 
   def set_board
     fill_pawns
-    fill_white_pieces
-    fill_black_pieces
+    %i[white black].each { |color| fill_pieces(color) }
   end
 
   def fill_pawns
-    8.times { |i| @chessboard[6][i] = Pawn.new(:white) }
-    8.times { |i| @chessboard[1][i] = Pawn.new }
+    8.times { |i| @chessboard[6][i] = ChessParser.create_chess_piece(:pawn, :white) }
+    8.times { |i| @chessboard[1][i] = ChessParser.create_chess_piece(:pawn, :black) }
   end
 
-  def fill_white_pieces
-    white_pieces = [Rook.new(:white), Knight.new(:white), Bishop.new(:white), King.new(:white),
-                    Queen.new(:white), Bishop.new(:white), Knight.new(:white), Rook.new(:white)]
-    white_pieces.each_with_index { |piece, index| @chessboard[7][index] = piece }
-  end
-
-  def fill_black_pieces
-    black_pieces = [Rook.new, Knight.new, Bishop.new, King.new,
-                    Queen.new, Bishop.new, Knight.new, Rook.new]
-    black_pieces.each_with_index { |piece, index| @chessboard[0][index] = piece }
+  def fill_pieces(color)
+    pieces = %i[rook knight bishop king queen bishop knight rook]
+    rank = color.eql?(:white) ? 7 : 0
+    pieces.each_with_index { |piece, index| @chessboard[rank][index] = ChessParser.create_chess_piece(piece, color) }
   end
 end
